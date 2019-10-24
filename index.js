2fc8b5f2baa1c558d5be615ca24d788939d423a1
Marcel Guerin <20742004+hungryogre@users.noreply.github.com>
2019-10-24 16:43:11 -0500

    mk module

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
 const Tree = require('./src/avltree');
-
-let t = new Tree();
-t.add('k');
-t.add('m');
-t.add('u');
-t.add('t');
-t.add('v');
-t.add('p');
-console.log('infix:');
-t.print();
-console.log('prefix:');
-t.print({ notation: "prefix" });
-console.log('postfix:');
-t.print({ notation: "postfix" });
+module.exports = Tree;
